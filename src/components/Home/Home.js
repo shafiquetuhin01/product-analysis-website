46771580a05ef3087017e0d6bf0c56cad50cdbef
Shafique <sfqtuhin04@gmail.com>
2022-04-04 08:32:02 +0600

    review section updating

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -24,10 +24,53 @@ const Home = () => {
               including the same 2560 x 1600 screen, Touch ID, 720p webcam,
               fingerprint sensor, and scissor-switch keyboard.
             </p>
+            <button className="btn btn-lg btn-secondary">Live Demo</button>
           </div>
           <div className="image-section">
             <img src={image} alt="" />
           </div>
+        </div>
+        <div className="customer-reviews mt-5">
+          <h2 className="mb-5 display-5 fw-bold text-center">
+            Customer Reviews
+          </h2>
+          <div className="review-items">
+            <div className="review text-left">
+              <h5>User: Abdul Basit</h5>
+              <p>This is an awesome laptop ever I have used.</p>
+              <h6>Ratings: 5</h6>
+            </div>
+            <div className="review text-left">
+              <h5>User: Mr.Henkin</h5>
+              <p>This laptop is really remarkable.</p>
+              <h6>Ratings: 5</h6>
+            </div>
+            <div className="review text-left">
+              <h5>User: Arnold Memphil</h5>
+              <p>Super fast but understanding not easy.</p>
+              <h6>Ratings: 4.5</h6>
+            </div>
+            <div className="review text-left">
+              <h5>User: Kawshik Armania</h5>
+              <p>I like it for my personal use.</p>
+              <h6>Ratings: 4.9</h6>
+            </div>
+            <div className="review text-left">
+              <h5>User: Robinson Gull</h5>
+              <p>I am searching for this type of product from a long time</p>
+              <h6>Ratings: 5</h6>
+            </div>
+            <div className="review text-left">
+              <h5>User: Mr Monowar</h5>
+              <p>Amezing Product ever I used.</p>
+              <h6>Ratings: 5</h6>
+            </div>
+          </div>
+          <h3 className="text-center">
+            <button className="btn btn-info btn-lg fw-bold mt-5">
+              See All Reviews
+            </button>
+          </h3>
         </div>
       </div>
       <div className="items-container"></div>
